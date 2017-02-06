abf04039ba00836fc46d7612a16c43db1c9353fb
mack0082 <mack.waters0803@gmail.com>
2017-02-06 13:41:02 -0500

    first commit

---
+++ Assignment2-1004/SharpAutoForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2_1004
+{
+    public partial class SharpAutoForm : Form
+    {
+        public SharpAutoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void claculateButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Create a new form
+            AboutForm aboutForm = new AboutForm();
+
+            //show the about form with dialog 
+            aboutForm.ShowDialog();
+        
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
